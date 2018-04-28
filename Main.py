398da48fbcb8122379525fcb8d4fcb883087907c
Richard Seaman <rdgseaman@gmail.com>
2018-04-28 12:16:28 +0000

    Added firebase culprits upload

--- Main.py
+++ Main.py
@@ -44,23 +44,57 @@ def take_picture(camera, destination_folder):
     camera.capture(destination_folder + image_file_name)
     return image_file_name
 
-#Read the temperature & humidty
+# Read the temperature & humidty
 def read_sensor():
-	try:
-                [temp,humidity] = grovepi.dht(temp_humidity_port,0)  # 0 for blue sensor
-                
-                return [temp,humidity]
-	
-	#Return -1 in case of sensor error
-	except IOError as TypeError:
-			return [-1,-1]
+    try:
+        [temp,humidity] = grovepi.dht(temp_humidity_port,0)  # 0 for blue sensor
+        
+        return [temp,humidity]
+    
+    #Return -1 in case of sensor error
+    except IOError as TypeError:
+        return [-1,-1]
+
+# Firebase uploads
+# Sensor readings
+def upload_sensor_readings(temperature, humidity):
+    # Construct the data to send to Firebase
+    # Use epock time as the key
+    # Add the average readings, plus a human readable date string
+    curr_time_sec=int(time.time())
+    timeKey = int(time.time())
+    data = {'timestamp': curr_time,'temperature': temperature, 'humidity': humidity}
+    result = firebase.put('/conditions', timeKey, data)
+    
+    # Log
+    print("Uploading to firebase:" + str(data))
+    #print(data)
+    
+    return result
+
+# Culboard Culprits
+def upload_culprit(imageName):
+    # Construct the data to send to Firebase
+    # Use epock time as the key
+    # Add the image name, plus a human readable date string
+    curr_time_sec=int(time.time())
+    timeKey = int(time.time())
+    data = {'timestamp': curr_time,'imageName': imageName}
+    result = firebase.put('/culprits', timeKey, data)
+    
+    # Log
+    print("Uploading to firebase:")
+    print(data)
+    
+    return result
 
 # Variable/Object definition before entering loop
 
 # Time to wait 
 time_between_checks = 5  # main loop delay
 time_between_sensor_reads = 10
-time_between_sensor_uploads = 40 * 0.5  
+time_between_sensor_uploads = 20 * 1  
+time_between_image_captures = 10 * 1  # ignore multiple opens in a row  
 
 # Variables for door open/close
 open_distance = 25
@@ -73,6 +107,7 @@ image_folder = get_folder("Images")
 #Save the initial time
 last_read_sensor= int(time.time())
 last_uploaded_readings= int(time.time())
+last_image_taken= int(time.time())
 
 # Variables for average readings
 # Each genuine reading between uploads will be added to this list
@@ -83,9 +118,6 @@ humidities = []
 
 # Firebase
 firebase = firebase.FirebaseApplication('https://cupboard-culprit.firebaseio.com', None)  # No auth for now
-#result = firebase.get('/test', 1)
-#result = firebase.post('/testing', 'testValue')
-#print(result)
 
 while True:
     
@@ -107,17 +139,24 @@ while True:
         
         print("Door open: %s" % door_open)
         
+        # Get the current time        
+        curr_time_sec=int(time.time())
+        
         # Check if this is the first time the door was opened, since it was closed
         # If so, take a picture
         if door_open and not door_was_open:
-            saved_image_name = take_picture(camera, image_folder)
-            print("Image saved: " + saved_image_name)
+            # Make sure sufficient time has passed since the previous capture
+            if curr_time_sec - last_image_taken > time_between_image_captures:
+                saved_image_name = take_picture(camera, image_folder)
+                print("Image saved: " + saved_image_name)
+                upload_culprit(saved_image_name)
+            else:
+                print("Image not taken as previous image was too recent.")
                
         # Remember door status for next time
         door_was_open = door_open
         
         # If it is time to take the sensor reading
-        curr_time_sec=int(time.time())
         curr_time = time.strftime("%Y-%m-%d:%H-%M-%S")
         if curr_time_sec - last_read_sensor > time_between_sensor_reads:
             [temp, humidity]=read_sensor()
@@ -125,9 +164,9 @@ while True:
             print(("Time:%s\nTemp: %.2f\nHumidity:%.2f %%\n" %(curr_time,temp,humidity)))
             
             # Check if readings are genuine and add to list if they are
-            if temp != -1 and temp != 0:
+            if temp != -1 and (temp >= 0.01 or temp <= 0.01):
                 temperatures.append(temp)
-            if humidity != -1 and humidity != 0:
+            if humidity != -1 and (humidity >= 0.01 or humidity <= 0.01):
                 humidities.append(humidity)
             
             #Update the last read time
@@ -136,26 +175,22 @@ while True:
         # Calculate and upload average if sufficient time has passed
         if curr_time_sec - last_uploaded_readings > time_between_sensor_uploads:
         
-            # Calculate the averages
-            avgTemp = sum(temperatures) / len(temperatures)
-            avgHum = sum(humidities) / len(humidities)
-            
-            # Construct the data to send to Firebase
-            # Use epock time as the key
-            # Add the average readings, plus a human readable date string
-            timeKey = int(time.time())
-            data = {'timestamp': curr_time,'temperature': avgTemp, 'humidity': avgHum}
-            result = firebase.put('/conditions', timeKey, data)
-            
-            # Log
-            print(("Time:%s\nAverage Temp: %.2f\nAverage Hum: %.2f" %(curr_time,avgTemp, avgHum)))
-            
-            # Reset the lists
-            temperatures = []
-            humidities = []
-            
-            #Update the last read time
-            last_uploaded_readings=curr_time_sec
+            # Make sure we have some readings
+            if len(temperatures) > 0 and len(humidities) > 0:
+                
+                # Calculate the averages
+                avgTemp = sum(temperatures) / len(temperatures)
+                avgHum = sum(humidities) / len(humidities)
+                
+                # Upload to firebase
+                result = upload_sensor_readings(avgTemp, avgHum)
+                
+                # Reset the lists
+                temperatures = []
+                humidities = []
+                
+                #Update the last read time
+                last_uploaded_readings=curr_time_sec
         
         #Slow down the loop
         time.sleep(time_between_checks)
